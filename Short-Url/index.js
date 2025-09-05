efd471525175e19b3ff1827e8daafa9cdbdbf207
Aayush Gupta <aayushgupta110418@gmail.com>
2025-09-05 21:19:23 +0530

    URL shortner

---
+++ Short-Url/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
+const {connectToMongoDb} = require("./connect");
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDb("mongodb://127.0.0.1:27017/short-url").then(()=> console.log("MongoDB connected!!"));
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get('/:shortId', async (req, res)=>{
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    }, { $push: {
+        visitHistory: {
+            timestamp: Date.now(),
+        }
+    }})
+    res.redirect(entry.redirectUrl);
+})
+
+app.listen(PORT, ()=> console.log(`Server Started at PORT ${PORT}`));
